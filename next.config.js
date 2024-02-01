0c7a7731b5dde67d4c7a0d7f6909d1067229385b
BjarneJensen <idsm@umich.edu>
2024-02-01 02:18:46 -0500

    t1

--- next.config.js
+++ next.config.js
@@ -1,56 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
-  images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
-    ],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
-})
-
-const isProd = process.env.NODE_ENV==='production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  // basePath: isProd ? "/nexttest":'',
-  output: "export",
-  distDir: "out",
-  // assetPrefix: process.env.ASSET_PREFIX,
-  // basePath: process.env.BASE_PATH,
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
   basePath: "/nexttest",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
