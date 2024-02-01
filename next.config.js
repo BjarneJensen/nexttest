52fd5fc59423812461ee32283169a26ee996009d
BjarneJensen <idsm@umich.edu>
2024-02-01 01:36:32 -0500

    t1

--- next.config.js
+++ next.config.js
@@ -30,17 +30,14 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  // basePath: isProd ? "/nexttest":'',
   output: "export",
-  distDir: "dist",
-  assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATH,
+
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  // basePath: "/nextjs-github-pages",
+  basePath: "/nexttest",
 
   /**
    * Disable server-based image optimization. Next.js does not support
